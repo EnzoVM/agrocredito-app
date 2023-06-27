b3a4a049fd5a8ff2832041070aff375af90e21b9
jemgdev <jemgdev@gmail.com>
2023-06-26 21:49:36 -0500

    Remove verify tokens in middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server'
-import { generateAccessTokenService, verifyAccessTokenService } from './services/auth.service'
  
-export default async function middleware (request: NextRequest) {
+export default function middleware (request: NextRequest) {
   const accessToken = request.cookies.get('accessToken')?.value
   const refreshToken = request.cookies.get('refreshToken')?.value
 
@@ -11,23 +10,7 @@ export default async function middleware (request: NextRequest) {
     return NextResponse.redirect(url)
   }
 
-  const { isValid: isAccessTokenValid } = await verifyAccessTokenService({ accessToken: accessToken! })
-
-  if (isAccessTokenValid) {
-    NextResponse.next()
-    return
-  }
-
-  const { isLoged } = await generateAccessTokenService({ refreshToken: refreshToken! })
-
-  if (isLoged) {
-    NextResponse.next()
-    return
-  }
-
-  const url = request.nextUrl.clone()
-  url.pathname = '/login'
-  return NextResponse.redirect(url)
+  NextResponse.next()
 }
 
 export const config = {
