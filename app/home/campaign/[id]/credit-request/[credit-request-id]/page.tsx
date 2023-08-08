c428eeec1cc6544f57d4ba3bea7a061bdbbcab5d
jemgdev <jemgdev@gmail.com>
2023-08-08 09:02:28 -0500

    Return error message when try to update credit request status to refused when this has one or more deliveries

--- app/home/campaign/[id]/credit-request/[credit-request-id]/page.tsx
+++ app/home/campaign/[id]/credit-request/[credit-request-id]/page.tsx
@@ -44,6 +44,7 @@ export default function CreditRequestDetail ({ params }: { params: { 'credit-req
   const [isLoading, setIsLoading] = useState(true)
   const [editable, setEditable] = useState(false)
   const [creditRequestState, setCreditRequestState] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
   
   useEffect(() => {
     getCreditRequestService({ creditRequestId: params['credit-request-id'] })
@@ -63,7 +64,7 @@ export default function CreditRequestDetail ({ params }: { params: { 'credit-req
     setCreditRequestState(creditRequestStateGot)
   }
 
-  const handleEditCreditRequesStatus = () => {
+  const handleEditCreditRequesStatus = async () => {
     try {
       if (!editable) {
         setEditable(!editable)
@@ -75,10 +76,10 @@ export default function CreditRequestDetail ({ params }: { params: { 'credit-req
         return
       }
 
-      updateCreditRequestStatusService({ creditRequestId: params["credit-request-id"], creditRequestStatus: creditRequestState })
+      await updateCreditRequestStatusService({ creditRequestId: params["credit-request-id"], creditRequestStatus: creditRequestState })
       setEditable(!editable)
-    } catch (error) {
-      console.log(error)
+    } catch (error: any) {
+      setErrorMessage(error.message)
     }
   }
   
@@ -103,6 +104,17 @@ export default function CreditRequestDetail ({ params }: { params: { 'credit-req
                     <button type="button" onClick={handleEditCreditRequesStatus} className="px-3 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">{editable ? 'Guardar' : 'Editar'}</button>
                   </div>
                   <p className="text-md tracking-tight text-gray-900 dark:text-white">Estado de la solicitud:</p>
+                  {
+                    errorMessage !== ''
+                      ? <div className="flex p-4 mb-4 text-sm text-red-800 border border-red-300 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 dark:border-red-800" role="alert">
+                          <svg aria-hidden="true" className="flex-shrink-0 inline w-5 h-5 mr-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd"></path></svg>
+                          <span className="sr-only">Info</span>
+                          <div>
+                            <span className="font-medium">Algo salió mal!</span> {errorMessage}
+                          </div>
+                        </div>
+                      : <></>
+                  }
                   {
                     editable
                       ? <select name="creditRequestState" onChange={handleChangeCreditRequestStatus} className="mb-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
