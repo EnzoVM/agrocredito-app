dea39269414d403d8e65d0c2b3b5b18b8e68b6be
jemgdev <jemgdev@gmail.com>
2023-08-20 15:26:56 -0500

    Repair datetime

--- app/components/delivery/CreateDelivery.tsx
+++ app/components/delivery/CreateDelivery.tsx
@@ -236,7 +236,7 @@ export default function CreateDelivery ({campaignId, setToggleCreate}:{campaignI
                 <div className="mb-6 w-full">
                   <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Fecha de entrega:</label>
                   <input
-                    type="date"
+                    type="datetime-local"
                     placeholder="Ingrese el objeto del prestamo" 
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
                     name="deliveryDatetime"
